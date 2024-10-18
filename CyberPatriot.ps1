feb05518c4d00f479ec48ec47d28b6499f1f4587
yougotfathead2 <ralbright1278@madison.k12.al.us>
2024-10-18 14:19:21 -0500

    CyberPatriot.ps1

---
+++ CyberPatriot.ps1
@@ -0,0 +1,17 @@
+# Users and Locate
+if (get-localuser -name Administrator)
+  {rename-localuser Administrator Ex-Administrator}
+if (get-localuser -name Guest)
+  {rename-localuser Guest Ex-Guest}
+
+
+# Import Audit Policy 
+auditpol /restore /file:.\audit.csv
+# .\ is the current directory
+
+
+# Import Password Policies/Lockout
+secedit /import /cfg:.\PasswordPolicies.inf
+
+
+# 
